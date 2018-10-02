708003f1f3ee1cb13428b321e88e4bfc7445e014
umview <umview@163.com>
2018-10-02 10:27:44 +0800

    OpticalFlowSensor-based-on-ADNS3080-with-arduino-and-Ultrasonic

---
+++ ArduinoPixFlow.ino
@@ -0,0 +1,371 @@
+float SDS_OutData[4];
+unsigned short CRC_CHECK(unsigned char *Buf, unsigned char CRC_CNT)
+{
+    unsigned short CRC_Temp;
+    unsigned char i,j;
+    CRC_Temp = 0xffff;
+
+    for (i=0;i<CRC_CNT; i++){
+        CRC_Temp ^= Buf[i];
+        for (j=0;j<8;j++) {
+            if (CRC_Temp & 0x01)
+                CRC_Temp = (CRC_Temp >>1 ) ^ 0xa001;
+            else
+                CRC_Temp = CRC_Temp >> 1;
+        }
+    }
+    return(CRC_Temp);
+}
+
+void SDS_OutPut_Data(float S_Out[])
+{
+  int temp[4] = {0};
+  unsigned int temp1[4] = {0};
+  unsigned char databuf[10] = {0};
+  unsigned char i;
+  unsigned short CRC16 = 0;
+ // float SDS_OutData[4];
+  /*for(i=0;i<4;i++) {
+  SDS_OutData[i]=S_Out[i];
+  }*/
+  for(i=0;i<4;i++)
+   {
+
+    temp[i]  = (int)SDS_OutData[i];
+    temp1[i] = (unsigned int)temp[i];
+
+   }
+
+  for(i=0;i<4;i++)
+  {
+    databuf[i*2]   = (unsigned char)(temp1[i]%256);
+    databuf[i*2+1] = (unsigned char)(temp1[i]/256);
+  }
+
+  CRC16 = CRC_CHECK(databuf,8);
+  databuf[8] = CRC16%256;
+  databuf[9] = CRC16/256;
+
+  //SDS_UART_Init();
+  for(i=0;i<10;i++)
+  {
+
+    Serial.write(databuf[i]);
+  }
+}
+/************************/
+#include "SPI.h"
+#define PIN_SS        10
+#define PIN_MISO      12
+#define PIN_MOSI      11
+#define PIN_SCK       13
+
+#define PIN_MOUSECAM_RESET     9
+#define PIN_MOUSECAM_CS        8
+
+#define ADNS3080_PIXELS_X                 30
+#define ADNS3080_PIXELS_Y                 30
+
+#define ADNS3080_PRODUCT_ID            0x00
+#define ADNS3080_REVISION_ID           0x01
+#define ADNS3080_MOTION                0x02
+#define ADNS3080_DELTA_X               0x03
+#define ADNS3080_DELTA_Y               0x04
+#define ADNS3080_SQUAL                 0x05
+#define ADNS3080_PIXEL_SUM             0x06
+#define ADNS3080_MAXIMUM_PIXEL         0x07
+#define ADNS3080_CONFIGURATION_BITS    0x0a
+#define ADNS3080_EXTENDED_CONFIG       0x0b
+#define ADNS3080_DATA_OUT_LOWER        0x0c
+#define ADNS3080_DATA_OUT_UPPER        0x0d
+#define ADNS3080_SHUTTER_LOWER         0x0e
+#define ADNS3080_SHUTTER_UPPER         0x0f
+#define ADNS3080_FRAME_PERIOD_LOWER    0x10
+#define ADNS3080_FRAME_PERIOD_UPPER    0x11
+#define ADNS3080_MOTION_CLEAR          0x12
+#define ADNS3080_FRAME_CAPTURE         0x13
+#define ADNS3080_SROM_ENABLE           0x14
+#define ADNS3080_FRAME_PERIOD_MAX_BOUND_LOWER      0x19
+#define ADNS3080_FRAME_PERIOD_MAX_BOUND_UPPER      0x1a
+#define ADNS3080_FRAME_PERIOD_MIN_BOUND_LOWER      0x1b
+#define ADNS3080_FRAME_PERIOD_MIN_BOUND_UPPER      0x1c
+#define ADNS3080_SHUTTER_MAX_BOUND_LOWER           0x1e
+#define ADNS3080_SHUTTER_MAX_BOUND_UPPER           0x1e
+#define ADNS3080_SROM_ID               0x1f
+#define ADNS3080_OBSERVATION           0x3d
+#define ADNS3080_INVERSE_PRODUCT_ID    0x3f
+#define ADNS3080_PIXEL_BURST           0x40
+#define ADNS3080_MOTION_BURST          0x50
+#define ADNS3080_SROM_LOAD             0x60
+
+#define ADNS3080_PRODUCT_ID_VAL        0x17
+
+void mousecam_reset()
+{
+  digitalWrite(PIN_MOUSECAM_RESET,HIGH);
+  delay(1); // reset pulse >10us
+  digitalWrite(PIN_MOUSECAM_RESET,LOW);
+  delay(35); // 35ms from reset to functional
+}
+
+int mousecam_init()
+{
+  pinMode(PIN_MOUSECAM_RESET,OUTPUT);
+  pinMode(PIN_MOUSECAM_CS,OUTPUT);
+  
+  digitalWrite(PIN_MOUSECAM_CS,HIGH);
+  
+  mousecam_reset();
+  
+  int pid = mousecam_read_reg(ADNS3080_PRODUCT_ID);
+  if(pid != ADNS3080_PRODUCT_ID_VAL)
+    return -1;
+
+  // turn on sensitive mode
+  mousecam_write_reg(ADNS3080_CONFIGURATION_BITS, 0x19);
+
+  return 0;
+}
+
+void mousecam_write_reg(int reg, int val)
+{
+  digitalWrite(PIN_MOUSECAM_CS, LOW);
+  SPI.transfer(reg | 0x80);
+  SPI.transfer(val);
+  digitalWrite(PIN_MOUSECAM_CS,HIGH);
+  delayMicroseconds(50);
+}
+
+int mousecam_read_reg(int reg)
+{
+  digitalWrite(PIN_MOUSECAM_CS, LOW);
+  SPI.transfer(reg);
+  delayMicroseconds(75);
+  int ret = SPI.transfer(0xff);
+  digitalWrite(PIN_MOUSECAM_CS,HIGH); 
+  delayMicroseconds(1);
+  return ret;
+}
+
+struct MD
+{
+ byte motion;
+ char dx, dy;
+ byte squal;
+ word shutter;
+ byte max_pix;
+};
+
+void mousecam_read_motion(struct MD *p)
+{
+  digitalWrite(PIN_MOUSECAM_CS, LOW);
+  SPI.transfer(ADNS3080_MOTION_BURST);
+  delayMicroseconds(75);
+  p->motion =  SPI.transfer(0xff);
+  p->dx =  SPI.transfer(0xff);
+  p->dy =  SPI.transfer(0xff);
+  p->squal =  SPI.transfer(0xff);
+  p->shutter =  SPI.transfer(0xff)<<8;
+  p->shutter |=  SPI.transfer(0xff);
+  p->max_pix =  SPI.transfer(0xff);
+  digitalWrite(PIN_MOUSECAM_CS,HIGH); 
+  delayMicroseconds(5);
+}
+
+// pdata must point to an array of size ADNS3080_PIXELS_X x ADNS3080_PIXELS_Y
+// you must call mousecam_reset() after this if you want to go back to normal operation
+int mousecam_frame_capture(byte *pdata)
+{
+  mousecam_write_reg(ADNS3080_FRAME_CAPTURE,0x83);
+  
+  digitalWrite(PIN_MOUSECAM_CS, LOW);
+  
+  SPI.transfer(ADNS3080_PIXEL_BURST);
+  delayMicroseconds(50);
+  
+  int pix;
+  byte started = 0;
+  int count;
+  int timeout = 0;
+  int ret = 0;
+  for(count = 0; count < ADNS3080_PIXELS_X * ADNS3080_PIXELS_Y; )
+  {
+    pix = SPI.transfer(0xff);
+    delayMicroseconds(10);
+    if(started==0)
+    {
+      if(pix&0x40)
+        started = 1;
+      else
+      {
+        timeout++;
+        if(timeout==100)
+        {
+          ret = -1;
+          break;
+        }
+      }
+    }
+    if(started==1)
+    {
+      pdata[count++] = (pix & 0x3f)<<2; // scale to normal grayscale byte range
+    }
+  }
+
+  digitalWrite(PIN_MOUSECAM_CS,HIGH); 
+  delayMicroseconds(14);
+  
+  return ret;
+}
+
+char asciiart(int k)
+{
+  static char foo[] = "WX86*3I>!;~:,`. ";
+  return foo[k>>4];
+
+
+  Serial.begin(9600);
+}
+/************************ADD************************/
+/*********************For Ultrasonic***********/
+#define TrigPin 6
+#define EchoPin 7
+//float distance; 
+struct DataStruct{
+ int16_t X;
+ int16_t Y;
+ uint16_t H;
+}Data;
+/*********for adns3080*************/
+byte frame[ADNS3080_PIXELS_X * ADNS3080_PIXELS_Y];
+void Capture(){
+   // if enabled this section grabs frames and outputs them as ascii art
+  
+  if(mousecam_frame_capture(frame)==0)
+  {
+    int i,j,k;
+    for(i=0, k=0; i<ADNS3080_PIXELS_Y; i++) 
+    {
+      for(j=0; j<ADNS3080_PIXELS_X; j++, k++) 
+      {
+        Serial.print(asciiart(frame[k]));
+        Serial.print(' ');
+      }
+      Serial.println();
+    }
+  }
+  Serial.println();
+  //delay(50); 
+}
+void getXY(struct DataStruct *data){
+    // if enabled this section produces a bar graph of the surface quality that can be used to focus the camera
+  // also drawn is the average pixel value 0-63 and the shutter speed and the motion dx,dy.
+  
+  //int val = mousecam_read_reg(ADNS3080_PIXEL_SUM);
+  MD md;
+  
+  mousecam_read_motion(&md);
+  /*
+  for(int i=0; i<md.squal/4; i++)
+    Serial.print('*');
+  Serial.print(' ');
+  Serial.print((val*100)/351);
+  Serial.print(' ');
+  
+  Serial.print(md.shutter); Serial.print(" (");
+*/
+//Serial.println(val);
+  (*data).X=md.dx;
+  (*data).Y=md.dy;
+  //return data;
+  /*
+  int X = (int)md.dx;
+  int Y = (int)md.dy;
+
+  SDS_OutData[0]=X;
+  SDS_OutData[1]=Y;
+  SDS_OutPut_Data(SDS_OutData);
+  */
+}
+int distance;
+void Filter(){
+  static float dis[5];
+  dis[0]=dis[1];dis[1]=dis[2];dis[2]=dis[3];dis[3]=dis[4];dis[4]=distance;
+  distance=(dis[0]+dis[1]+dis[2]+dis[3]+dis[4])*0.2;
+}
+void getH(struct DataStruct *data){
+  digitalWrite(TrigPin, LOW); 
+  delayMicroseconds(2); 
+  digitalWrite(TrigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(TrigPin, LOW); 
+    // 检测脉冲宽度，并计算出距离
+    //SDS_OutData[2]=pulseIn(EchoPin,HIGH,10000);
+  (*data).H = (int)(pulseIn(EchoPin,HIGH,40000)/ 58.00);
+}
+
+/*************************************MAIN*********************************/
+#include <SoftwareSerial.h>
+#include <MsTimer2.h>
+void Sample(){
+  getXY(&Data);
+  getH(&Data);
+  Filter();
+
+}
+SoftwareSerial COM(5,4); // RX, TX
+void setup() 
+{
+  pinMode(PIN_SS,OUTPUT);
+  pinMode(PIN_MISO,INPUT);
+  pinMode(PIN_MOSI,OUTPUT);
+  pinMode(PIN_SCK,OUTPUT);
+  /********************/
+  pinMode(TrigPin, OUTPUT); 
+  pinMode(EchoPin, INPUT); 
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV32);
+  SPI.setDataMode(SPI_MODE3);
+  SPI.setBitOrder(MSBFIRST);
+  
+  Serial.begin(115200);
+  COM.begin(115200);
+  if(mousecam_init()==-1)
+  {
+    Serial.println("Mouse cam failed to init");
+    while(1);
+  }
+  //MsTimer2::set(10, Sample); // 500ms period
+  //MsTimer2::start();
+}
+#define LIMIT( x,min,max ) ( (x) < (min)  ? (min) : ( (x) > (max) ? (max) : (x) ) )
+void loop() 
+{
+  getXY(&Data);
+  getH(&Data);
+  Filter();
+  Data.X=LIMIT(Data.X,-127,127);
+  Data.Y=LIMIT(Data.Y,-127,127);
+  Data.H=LIMIT(Data.H,0,255);
+  COM.write((uint8_t)0);
+  COM.write((uint8_t)255);
+  COM.write((uint8_t)Data.X);
+  COM.write((uint8_t)Data.Y);
+  COM.write((uint8_t)Data.H);
+  COM.write((uint8_t)255);
+  COM.write((uint8_t)0);
+  SDS_OutData[0]=Data.X;
+  SDS_OutData[1]=Data.Y;
+  SDS_OutData[2]=Data.H;
+  SDS_OutPut_Data(SDS_OutData);
+/*
+  Serial.write((uint8_t)0);
+  Serial.write((uint8_t)255);
+  Serial.write((uint8_t)Data.X);
+  Serial.write((uint8_t)Data.Y);
+  Serial.write((uint8_t)Data.H);
+  Serial.write((uint8_t)255);
+  Serial.write((uint8_t)0);
+  */
+  delay(20);
+}
